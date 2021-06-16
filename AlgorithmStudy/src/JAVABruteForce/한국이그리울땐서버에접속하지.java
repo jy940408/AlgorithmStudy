d192df4c3fce58fecd4609500a47865b6c77b7a7
jy940408 <junyoung940408@gmail.com>
2021-06-16 22:54:40 +0900

    20211616_알고리즘스터디_브루트포스_백준_9996_한국이그리울땐서버에접속하지_2

--- AlgorithmStudy/src/JAVABruteForce/한국이그리울땐서버에접속하지.java
+++ AlgorithmStudy/src/JAVABruteForce/한국이그리울땐서버에접속하지.java
@@ -23,29 +23,75 @@ public class 한국이그리울땐서버에접속하지 {
 
 		for(int i = 0 ; i < amountNum ; i++) {
 			ArrayList<String> testString = new ArrayList<String>(Arrays.asList(testLine[i].split("")));
-			
-			root1:
-			for(int j = 0, k = 0 ; j < patternString.size() ; j++, k++) {
-				if(!patternString.get(j).equals(testString.get(k)) && !(patternString.get(j)).equals("*")) {
-					resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 1 NE";
-					break;
-				}else if((patternString.get(j)).equals("*") && j == (patternString.size()-1)) {
-					resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 2 DA";
-					break;
-				}else if((patternString.get(j)).equals("*") && j != (patternString.size()-1)) {
-					for(int l = 0 ; l < testString.size()-k ; l++ ) {
-						if(patternString.get(j+1).equals(testString.get(k+l))) {
-							System.out.println(k + " " + l);
-							k = k + l-1;
+			if(patternString.size() > testString.size()) {
+				root1:
+				for(int j = 0, k = 0 ; j < patternString.size() ; j++, k++) {
+					if(k < testString.size()) {
+						if(!patternString.get(j).equals(testString.get(k)) && !(patternString.get(j)).equals("*")) {
+							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 1 NE";
 							break;
-						}else if(l == ((testString.size()-k)-1) && !patternString.get(j+1).equals(testString.get(k+l))) {
-							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) +  " 4 NE";
-							break root1;
+						}else if((patternString.get(j)).equals("*") && j == (patternString.size()-1)) {
+							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 2 DA";
+							break;
+						}else if((patternString.get(j)).equals("*") && j != (patternString.size()-1)) {
+							for(int l = 0 ; l < testString.size()-k ; l++ ) {	
+								if(patternString.get(j+1).equals(testString.get(k+l))) {
+									System.out.println(k + " " + l);
+									k = k + l-1;
+									break;
+								}else if(l == ((testString.size()-k)-1) && !patternString.get(j+1).equals(testString.get(k+l))) {
+									resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) +  " 4 NE";
+									break root1;
+								}
+							}
+						}else if(patternString.get(j).equals(testString.get(k)) && j == (patternString.size()-1)) {
+							if(k == testString.size()-1) {
+								resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 5 DA";
+								break;
+							}else {
+								resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 12 NE";
+								break;
+							}
+						}
+					}else if(k >= testString.size()) {
+						if(j == patternString.size()-1 && patternString.get(j).equals("*")) {
+							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + " 6 DA";
+							break;
+						}else {
+							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + " 7 NE";
+							break;
 						}
 					}
-				}else if(patternString.get(j).equals(testString.get(k)) && j == (patternString.size()-1)) {
-					resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 5 DA";
-					break;
+				}
+			}else if(patternString.size() <= testString.size()) {
+				root1:
+				for(int j = 0, k = 0 ; j < patternString.size() ; j++, k++) {
+					if(!patternString.get(j).equals(testString.get(k)) && !(patternString.get(j)).equals("*")) {
+						resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 1 NE";
+						break;
+					}else if((patternString.get(j)).equals("*") && j == (patternString.size()-1)) {
+						resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 2 DA";
+						break;
+					}else if((patternString.get(j)).equals("*") && j != (patternString.size()-1)) {
+						for(int l = 0 ; l < testString.size()-k ; l++ ) {	
+							if(patternString.get(j+1).equals(testString.get(k+l))) {
+								System.out.println(k + " " + l);
+								k = k + l-1;
+								break;
+							}else if(l == ((testString.size()-k)-1) && !patternString.get(j+1).equals(testString.get(k+l))) {
+								resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) +  " 4 NE";
+								break root1;
+							}
+						}
+					}else if(patternString.get(j).equals(testString.get(k)) && j == (patternString.size()-1)) {
+						if(k == testString.size()-1) {
+							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 5 DA";
+							break;
+						}else {
+							resultList[i] = j + " " + patternString.get(j) + " " + k + " " + testString.get(k) + " 12 NE";
+							break;
+						}
+					}
 				}
 			}
 			
