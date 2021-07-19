c6d5e31ebb8828adff0d908398e1116cc438cf79
jy940408 <junyoung940408@gmail.com>
2021-07-19 18:18:27 +0900

    20210719_알고리즘스터디_백트래킹_백준_15651_N과M(3)_3

--- AlgorithmStudy/src/JAVABackTracking/N과M3.java
+++ AlgorithmStudy/src/JAVABackTracking/N과M3.java
@@ -1,9 +1,10 @@
 package JAVABackTracking;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
 public class N과M3 {
 
@@ -11,11 +12,11 @@ public class N과M3 {
 	
 	public static void main(String[] args) throws IOException {
 
-		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String[] firstLine = (scan.nextLine()).split(" ");
+		String[] firstLine = (br.readLine()).split(" ");
 		
-		scan.close();
+		br.close();
 		
 		int lastNum = Integer.parseInt(firstLine[0]);
 		int amount = Integer.parseInt(firstLine[1]);
@@ -24,6 +25,8 @@ public class N과M3 {
 		int[] result = new int[amount];
 		
 		count(lastNum, amount, result, count);
+		
+		bw.flush();
 		
 	}
 	
@@ -36,14 +39,13 @@ public class N과M3 {
 			}
 			
 			bw.newLine();
-			bw.flush();
-			
 			return;
 		}
 		
+		
+		
 		for(int i = 1 ; i <= lastNum ; i++) {
-			
-			System.out.println("amount: " + amount + ", count: " + count);
+
 			result[count] = i;
 			count(lastNum, amount, result, count + 1);
 			
