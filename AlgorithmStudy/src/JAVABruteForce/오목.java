944eaf4c069e727d4ff034ad1e4ce5419d77001b
jy940408 <junyoung940408@gmail.com>
2021-07-22 18:54:05 +0900

    20210722_알고리즘스터디_브루트포스_백준_2615_오목_3

--- AlgorithmStudy/src/JAVABruteForce/오목.java
+++ AlgorithmStudy/src/JAVABruteForce/오목.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 
 public class 오목 {
 
+	static int[][] board = new int[19][19];
+	static int black, white, row, col = 0;
+	
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		
-		int[][] board = new int[19][19];
-		int black, white = 0;
 		
 		ArrayList test = new ArrayList();
 		
@@ -34,12 +34,158 @@ public class 오목 {
 		
 		scan.close();
 		
-		for(int i = 0 ; i < 18 ; i++) {
-			for(int j = 0 ; j < 18 ; j++) {
-				
+		for(int i = 0 ; i < 19 ; i++) {
+			for(int j = 0 ; j < 19 ; j++) {
+				if(board[i][j] != 0) {
+					check(i, j);
+				}
+			}
+		}
+		
+		System.out.println("black: " + black + ", white: " + white + ", row: " + row + ", col: " + col);
+		
+		result();
+		
+	}
+
+	public static void check(int i, int j) {
+		
+		if(i < 14 && j < 14) {
+			for(int k = 0 ; k < 5 ; k++) {
+	//			가로 확인
+				if(board[i][j] != board[i+k][j]) {
+					break;
+				}else if(board[i][j] == board[i+k][j] && k == 4) {
+					if(i < 13) {
+						if(board[i+k][j] != board[i+k+1][j]) {
+							row = i;
+							col = j;
+							if(board[i][j] == 1) {
+								black++;
+							}else {
+								white++;
+							}
+						}else{
+							break;
+						}
+					}else if(i == 13) {
+						row = i;
+						col = j;
+						if(board[i][j] == 1) {
+							black++;
+						}else {
+							white++;
+						}
+					}
+				}
+			}
+			
+			for(int k = 0 ; k < 5 ; k++) {
+	//			세로 확인
+				if(board[i][j] != board[i][j+k]) {
+					break;
+				}else if(board[i][j] == board[i][j+k] && k == 4) {
+					if(j < 13) {
+						if(board[i][j+k] != board[i][j+k+1]) {
+							row = i;
+							col = j;
+							if(board[i][j] == 1) {
+								black++;
+							}else {
+								white++;
+							}
+						}else{
+							break;
+						}
+					}else if(j == 13) {
+						row = i;
+						col = j;
+						if(board[i][j] == 1) {
+							black++;
+						}else {
+							white++;
+						}
+					}
+				}
+			}
+			
+			for(int k = 0 ; k < 5 ; k++) {
+	//			우하 대각 확인
+				if(board[i][j] != board[i+k][j+k]) {
+					break;
+				}else if(board[i][j] == board[i+k][j+k] && k == 4) {
+					if(i < 13 && j < 13) {
+						if(board[i+k][j+k] != board[i+k+1][j+k+1]) {
+							row = i;
+							col = j;
+							if(board[i][j] == 1) {
+								black++;
+							}else {
+								white++;
+							}
+						}else{
+							break;
+						}
+					}else if(i == 13 && j == 13) {
+						row = i;
+						col = j;
+						if(board[i][j] == 1) {
+							black++;
+						}else {
+							white++;
+						}
+					}
+				}
+			}
+		}
+
+		if(i > 5 && j < 14) {
+			for(int k = 0 ; k < 5 ; k++) {
+	//			우상 대각 확인
+				if(board[i][j] != board[i-k][j+k]) {
+					break;
+				}else if(board[i][j] == board[i-k][j+k] && k == 4) {
+					if(i > 4 && j < 13) {
+						if(board[i-k][j+k] != board[i-k-1][j+k+1]) {
+							row = i;
+							col = j;
+							if(board[i][j] == 1) {
+								black++;
+							}else {
+								white++;
+							}
+						}else{
+							break;
+						}
+					}else if(i == 4 && j == 13) {
+						row = i;
+						col = j;
+						if(board[i][j] == 1) {
+							black++;
+						}else {
+							white++;
+						}
+					}
+				}
 			}
 		}
 		
 	}
-
+	
+	public static void result() {
+		if(black == 1 && white < 1) {
+			
+			System.out.println(black + "\n" + (row+1) + " " + (col+1));
+			
+		}else if(white == 1 && black < 1) {
+			
+			System.out.println(white + "\n" + (row+1) + " " + (col+1));
+			
+		}else {
+			
+			System.out.println(0);
+			
+		}
+	}
+	
 }
