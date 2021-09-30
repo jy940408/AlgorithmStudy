ace755510bc2430e1009f3ebcbf531b011d20d28
jy940408 <79436393+jy940408@users.noreply.github.com>
2021-09-30 23:15:28 +0900

    20210930_알고리즘스터디_브루트포스_백준_14225_부분수열의합

--- AlgorithmStudy/src/JAVABruteForce/부분수열의합.java
+++ AlgorithmStudy/src/JAVABruteForce/부분수열의합.java
@@ -18,51 +18,50 @@ public class 부분수열의합 {
 	public static void main(String[] args) throws IOException{
 	
 		int arrayList = Integer.parseInt(br.readLine());
-		String[] array_ = (br.readLine()).split(" ");
-		int[] array = new int[arrayList];
-		boolean[] visitList = new boolean[arrayList];
-		for(int i = 0 ; i < arrayList ; i++) {
-			array[i] = Integer.parseInt(array_[i]);
-			visitList[i] = true;
-		}
-		
-		for(int i = 1 ; i < arrayList+1 ; i++) {
-			ArrayList<Integer> btList = new ArrayList<Integer>();
-			bt(array, btList, visitList, 0, i, 0);
-		}
-		
-		int resultTest = 1;
-		while(resultList.containsKey(resultTest)) {
-			resultTest++;
-		}
-		
-		br.close();
-		bw.write(resultTest + "");
-		bw.flush();
-		bw.close();
-		
+ 		String[] array_ = (br.readLine()).split(" ");
+ 		int[] array = new int[arrayList];
+ 		boolean[] visitList = new boolean[arrayList];
+ 		for(int i = 0 ; i < arrayList ; i++) {
+ 			array[i] = Integer.parseInt(array_[i]);
+ 			visitList[i] = true;
+ 		}
+
+ 		for(int i = 1 ; i < arrayList+1 ; i++) {
+ 			ArrayList<Integer> btList = new ArrayList<Integer>();
+ 			bt(array, btList, visitList, 0, i, 0);
+ 		}
+
+ 		int resultTest = 1;
+ 		while(resultList.containsKey(resultTest)) {
+ 			resultTest++;
+ 		}
+
+ 		br.close();
+ 		bw.write(resultTest + "");
+ 		bw.flush();
+ 		bw.close();
+	 		
 	}
 	
 	private static void bt(int[] array, ArrayList<Integer> btList, boolean[] visitList, int depth, int length, int postIdx) {
 		if(depth == length) {
-			int result = 0;
-			for(int i = 0 ; i < btList.size() ; i++) {
-				result += btList.get(i);
-			}
-			resultList.put(result, 1);
-			return;
-		}
-		
-		for(int i = depth ; i < array.length ; i++) {
-			if(visitList[i] && i >= postIdx) {
-				visitList[i] = false;
-				btList.add(depth, array[i]);
-				bt(array, btList, visitList, depth+1, length, i);
-				btList.remove(depth);
-				visitList[i] = true;
-			}
-		}
-		
-	}
+ 			int result = 0;
+ 			for(int i = 0 ; i < btList.size() ; i++) {
+ 				result += btList.get(i);
+ 			}
+ 			resultList.put(result, 1);
+ 			return;
+ 		}
 
-}
+ 		for(int i = depth ; i < array.length ; i++) {
+ 			if(visitList[i] && i >= postIdx) {
+ 				visitList[i] = false;
+ 				btList.add(depth, array[i]);
+ 				bt(array, btList, visitList, depth+1, length, i);
+ 				btList.remove(depth);
+ 				visitList[i] = true;
+ 			}
+ 		}
+ 	}
+
+ }
