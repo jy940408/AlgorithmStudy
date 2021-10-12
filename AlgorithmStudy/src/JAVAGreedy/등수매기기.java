53c36b2b0b9a7fc50f7a9b81967d64a93137a9ba
jy940408 <79436393+jy940408@users.noreply.github.com>
2021-10-12 22:22:13 +0900

    20211012_알고리즘스터디_그리디_백준_2012_등수매기기

---
+++ AlgorithmStudy/src/JAVAGreedy/등수매기기.java
@@ -0,0 +1,43 @@
+package JAVAGreedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 등수매기기 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String[] args) throws IOException{
+
+		int testNum = Integer.parseInt(br.readLine());
+		int[] testCase = new int[testNum];
+		for(int i = 0 ; i < testNum ; i++) {
+			testCase[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(testCase);
+		
+		long result = 0;
+		for(int i = 0 ; i < testNum ; i++) {
+			if((i+1) <= testCase[i]) {
+				result += testCase[i]-(i+1);
+			}else {
+				result += (i+1)-testCase[i];
+			}
+		}
+		
+		br.close();
+		bw.write(result + "");
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
