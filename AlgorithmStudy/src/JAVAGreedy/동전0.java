fb5bde206209bcfbc0f74793c2ad017cf623c595
jy940408 <79436393+jy940408@users.noreply.github.com>
2021-10-14 08:43:30 +0900

    20211014_알고리즘스터디_그리디_백준_11047_슬라임합치기

---
+++ AlgorithmStudy/src/JAVAGreedy/동전0.java
@@ -0,0 +1,41 @@
+package JAVAGreedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class 동전0 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws IOException{
+ 
+		String[] firstLine = (br.readLine()).split(" ");
+		int coinNum = Integer.parseInt(firstLine[0]);
+		int coinSum = Integer.parseInt(firstLine[1]);
+		
+		int[] coinArray = new int[coinNum];
+		for(int i = 0 ; i < coinNum ; i++) {
+			coinArray[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int result = 0;
+		for(int i = coinNum-1 ; i >= 0 ; i--) {
+			if(coinArray[i] <= coinSum) {
+				result += coinSum/coinArray[i];
+				coinSum = coinSum%coinArray[i];
+			}
+		}
+		
+		br.close();
+		bw.write(result + "");
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
