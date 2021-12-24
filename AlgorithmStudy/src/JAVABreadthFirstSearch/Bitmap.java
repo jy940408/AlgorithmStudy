03f970ede093ca2dbe48f3962df5eeec572d9a58
jy940408 <79436393+jy940408@users.noreply.github.com>
2021-12-24 22:13:49 +0900

    20211224_알고리즘스터디_너비우선탐색_백준_8061_Bitmap

---
+++ AlgorithmStudy/src/JAVABreadthFirstSearch/Bitmap.java
@@ -0,0 +1,73 @@
+package JAVABreadthFirstSearch;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Bitmap {
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] upDown = {-1, 1, 0, 0}, leftRight = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException{
+        
+        String[] firstLine = (br.readLine()).split(" ");
+        int height = Integer.parseInt(firstLine[0]);
+        int width = Integer.parseInt(firstLine[1]);
+
+        int[][] board = new int[height][width];
+        int[][] visitList = new int[height][width];
+        for(int i = 0 ; i < height ; i++){
+            for(int j = 0 ; j < width ; j++){
+                visitList[i][j] = -1;
+            }
+        }
+
+        Queue<int[]> bfsQueue = new LinkedList<>();
+        for(int i = 0 ; i < height ; i++){
+            String[] secondLine = (br.readLine()).split("");
+            for(int j = 0 ; j < width ; j++){
+                board[i][j] = Integer.parseInt(secondLine[j]);
+                if(board[i][j] == 1){
+                    visitList[i][j] = 0;
+                    bfsQueue.add(new int[] {i, j, 0});
+                }
+            }
+        }
+
+        while(!bfsQueue.isEmpty()){
+            int[] thisQueue = bfsQueue.poll();
+
+            for(int i = 0 ; i < 4 ; i++){
+                int subHeight = (thisQueue[0] + upDown[i]);
+                int subWidth = (thisQueue[1] + leftRight[i]);
+
+                if(subHeight >= 0 && subHeight < height && subWidth >= 0 && subWidth < width){
+                    if(visitList[subHeight][subWidth] == -1){
+                        visitList[subHeight][subWidth] = (thisQueue[2]+1);
+                        bfsQueue.add((new int[] {subHeight, subWidth, (thisQueue[2]+1)}));
+                    }
+                }
+            }
+        }
+
+        for(int i = 0 ; i < height ; i++){
+            String result = "";
+            for(int j = 0 ; j < width ; j++){
+                result += (Integer.toString(visitList[i][j]) + " ");
+            }
+            bw.write(result + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+    }
+
+}
